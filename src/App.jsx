6907dd9b2a5b74267d7375ef3e25a983fe93dfbe
Om Sharma <omsharma.83173@gmail.com>
2025-01-11 22:00:34 +0530

    added-email-password-authentication-using-firebase.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import { useState } from 'react'
-
-
+import {getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword} from "firebase/auth"
+import app from './config/firebase-config';
 function App() {
-  const [count, setCount] = useState(0)
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const auth = getAuth(app);
+  const signUp = async(email,password) =>{
+    try{
+      const userCredential = await createUserWithEmailAndPassword(auth,email,password);
+      console.log("User SignedUp, ",userCredential.user);
+      alert("SignUp Successful!")
+    }catch(error){
+      console.error("Error! SigningUp!",error)
+      alert("Error SignUp!")
+    }
+  }
 
+  const signIn = async(email,password) =>{
+    try{
+      const userCredential = await signInWithEmailAndPassword(auth,email,password);
+      console.log("User signin!",userCredential.user)
+      alert("SignIn Successful!")
+    }catch(error){
+      console.error("Error : ",error);
+      alert("SignIN Failed!")
+    }
+  }
   return (
     <>
-    hi
+    <input type="text" placeholder="Enter Your Email" onChange={(e)=>{
+      setEmail(e.target.value);
+      console.log(e.target.value);
+    }}/>
+    <input type="password" placeholder="Enter Your Password" onChange={(e) => {
+      setPassword(e.target.value);
+      console.log(e.target.value);
+    }}
+    />
+    <button onClick={()=>{
+      signUp(email,password)
+    }}>SignUp</button>
+
+    <button onClick={()=>{
+      signIn(email,password);
+    }}>SignIn</button>
     </>
 
   )
